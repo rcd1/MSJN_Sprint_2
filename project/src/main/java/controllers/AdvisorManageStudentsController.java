92788a76e826d82121395ac1c63280d219bb3e0c
Joy-lj <joyluya@gmail.com>
2024-04-19 15:44:35 -0400

    advisor manage students screens

---
+++ project/src/main/java/controllers/AdvisorManageStudentsController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import model.*;
+
+public class AdvisorManageStudentsController implements Initializable {
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private AnchorPane advisorAddStudent;
+
+    @FXML
+    private AnchorPane advisorViewStudent;
+
+    @FXML
+    private AnchorPane advisorRemoveStudent;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            handleTabSelection(newValue);
+        });
+    }
+
+    private void handleTabSelection(Tab selectedTab) {
+        if (selectedTab == null) {
+            return;
+        }
+
+        advisorAddStudent.setDisable(!selectedTab.getText().equals("Add"));
+        advisorViewStudent.setDisable(!selectedTab.getText().equals("View"));
+        advisorRemoveStudent.setDisable(!selectedTab.getText().equals("Remove"));
+    }
+}
--- project/src/main/java/msjn/TestLoader.java
+++ project/src/main/java/msjn/TestLoader.java
@@ -16,7 +16,7 @@ public class TestLoader extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("fillrequirementcoursesrefactor"), 600, 450);
+        scene = new Scene(loadFXML("advisorManageStudents"), 600, 450);
         stage.setScene(scene);
         stage.show();
     }
