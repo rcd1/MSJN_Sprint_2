edbb91f80a315fefe2f69512a6530a82b9388a3f
unknown <sj49@email.sc.edu>
2024-04-20 19:33:58 -0400

    stuff

--- project/src/main/java/controllers/ViewAppArea.java
+++ project/src/main/java/controllers/ViewAppArea.java
@@ -7,14 +7,41 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import msjn.App;
 
  public class ViewAppArea {
+    @FXML
+    private ResourceBundle resource;
 
-   
+    @FXML
+    private URL location;
+
+    @FXML
+    private RadioButton undeclaredRadioButton;
+
+    @FXML
+    private RadioButton scienceRadioButton;
+
+    @FXML
+    private RadioButton umathRadioButton;
+
+    @FXML
+    private RadioButton digitalDesignRadioButton;
+
+    @FXML
+    private RadioButton robotRadioButton;
+
+    @FXML
+    private RadioButton speechRadioButton;
+
+    @FXML
+    void buttonclicked(MouseEvent event) throws IOException {
+        System.out.println("Application Area selected");
+    }
 
 
 }
--- project/src/main/java/model/Test.java
+++ project/src/main/java/model/Test.java
@@ -8,11 +8,11 @@ public class Test {
     public static void main(String[] args) {
         UserList users = UserList.getInstance();
         ArrayList<Student> students = users.getStudents();
-        for (Student student : students) {
-            for (Note note : student.getNotes()) {
-                System.out.println(note.toString());
-            }
-        }
+         for (Student student : students) {
+             for (Note note : student.getNotes()) {
+                 System.out.println(note.toString());
+             }
+         }
         //new ApplicationUI().run();
     }
 }
